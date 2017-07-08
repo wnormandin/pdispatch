9b5d150d5880478df51e98c2894a461b03c3d846
William Normandin <normandindev@gmail.com>
2017-07-08 17:45:58 +0000

    Update pokeydispatch.py

--- pokeydispatch.py
+++ pokeydispatch.py
@@ -35,6 +35,7 @@ import sys
 import subprocess
 import argparse
 import textwrap as _textwrap
+import socket
 from socket import timeout as TOE
 
 try:
@@ -52,17 +53,35 @@ USAGE= '''
     soon.
 
     Usage Examples:
-        ## Basic connectivity test
+        * Basic connectivity test
         $ ./sshcmd.py --rsa=/path/to/id_rsa.pub --user=<username> --ip <destination IP> --cmd "uname -a"
 
-        ## Passwords may be passed, though keys are recommended
+        * Passwords may be passed, though keys are recommended
         $ ./sshcmd.py --user=<username> --password=<password> --ip <destination IP> --cmd "ls"
 
-        ## Multiple destination IPs are possible
+        * Multiple destination IPs are possible
         $ ./sshcmd.py --user=<username> --rsa=<rsa_pub> --ip 12.34.56.78:122 192.168.1.12:9998
 
-        ## If they all share a common port, use --port
+        * If they all share a common port, use --port
         $ ./sshcmd.py --user=<username> --rsa=<rsa_pub> --port=<common port> --ip 12.34.56.78 192.168.1.12
+
+        * To run as a server, pass --server.  Various arguments are handled differently:
+            --ip:       bind IP address
+            --port:     bind port
+            --rsa:      the host rsa key
+            --user:     client username
+            --passwd:   client password
+
+        * In server mode, the following arguments are ignored:
+            --cmd
+            --host-key-file
+            -i | --interactive
+            -r | --remote
+
+    Server Example:
+        * This example demonstrates basic server usage, listening @ 127.0.0.1:9998 for the user <username> with password <password>
+        $ ./pokeydispatch.py --rsa=/path/to/host_key --user=<username> --passwd=<password> --ip=127.0.0.1 --port=9998 --server -v
+
     '''
 
 def cli():
@@ -80,6 +99,7 @@ def cli():
     parser.add_argument('-r', '--remote', action='store_true', help='Execute commands sent by remote')
     parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output')
     parser.add_argument('-i','--interactive', action='store_true', help='Start an interactive session')
+    parser.add_argument('-d', '--debug', action='store_true', help='Raise exceptions')
     return parser.parse_args()
 
 
@@ -119,10 +139,38 @@ class Color:
     ERR = '\x1b[1;31;44m'
     TST = '\x1b[7;34;46m'
 
-class Server:
+class Server(paramiko.ServerInterface):
 
     def __init__(self):
-        pass
+        self.event = threading.Event()
+
+    def check_channel_request(self, kind, channel_id):
+        if kind == 'session':
+            return paramiko.OPEN_SUCCEEDED
+        return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+
+    def check_auth_password(self, username, password):
+        if username == args.user and password == args.passwd:
+            return paramiko.AUTH_SUCCESSFUL
+        return paramiko.AUTH_FAILED
+
+    def listen(self):
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            cprint(' -  Binding to host {}:{}'.format(args.ip, args.port), Color.BLUE, True)
+            self.sock.bind((args.ip, args.port))
+            self.sock.listen(100)
+            cprint(' -  Listening for connections', Color.BLUE, True)
+            client, c_addr = self.sock.accept()
+        except Exception as e:
+            if args.debug:
+                raise
+            cprint('[!] Listen failed: {}'.format(e), Color.ERR)
+            sys.exit(1)
+        else:
+            cprint('[*] Session initiated from {}'.format(c_addr), Color.MSG, True)
+            return client, c_addr
 
 class Client:
 
@@ -154,7 +202,6 @@ class Client:
                                                     ip, port), Color.ERR)
                     continue
                 cprint('[*] Connected to {}:{}'.format(ip, port), Color.GREEN, True)
-                self.ssh_session = self.client.get_transport().open_session()
                 self.session_run()
                 self.disconnect()
 
@@ -170,15 +217,21 @@ class Client:
         self.client.close()
 
     def session_run(self):
+        self.ssh_session = self.client.get_transport().open_session()
         if self.ssh_session.active:
             cprint(' - SSH session active', Color.BLUE, True)
             if args.cmd is not None:
-                self.ssh_session.exec_command(args.cmd)
-                cprint(self.ssh_session.recv(1024).decode().rstrip())
+                if args.remote:
+                    self.ssh_session.send(args.cmd)
+                    cprint(self.ssh_session.recv(1024).decode().rstrip())
+                else:
+                    self.ssh_session.exec_command(args.cmd)
             if args.interactive:
                 pass
             if args.remote:
                 while True:
+                    if not self.ssh_session.active:
+                        self.ssh_session = self.client.get_transport().open_session()
                     command = self.ssh_session.recv(1024)
                     if command == "DISCONNECT":
                         return
@@ -189,17 +242,64 @@ class Client:
                         self.ssh_session.send(str(e))
                 self.client.close()
 
+def run_server():
+    cprint('[*] Starting ssh server', Color.MSG)
+    assert len(args.ip) == 1, 'A single bind IP must be specified in server mode'
+    args.ip = args.ip[0]
+    app = Server()
+    client, c_addr = app.listen()
+    global sess
+    sess = paramiko.Transport(client)
+    sess.add_server_key(paramiko.RSAKey(filename=args.rsa))
+    cprint(' -  Server keys added', Color.BLUE, True)
+    try:
+        sess.start_server(server=app)
+        cprint(' -  Server started', Color.BLUE, True)
+    except paramiko.SSHException as e:
+        if args.debug:
+            raise
+        cprint('[!] SSH Negotiation failed', Color.ERR)
+    chan = sess.accept(20)
+    if chan is None:
+        raise AssertionError('Channel is None')
+    cprint('[*] Authenticated', Color.MSG)
+    cprint(chan.recv(1024).decode())
+    chan.send('Welcome to pokey_ssh')
+    while True:
+        try:
+            cprint(' -  Getting command', Color.BLUE, True)
+            cmd = input('Command: ').rstrip()
+            if cmd.lower() != 'exit' and cmd.lower() != 'quit':
+                chan.send(cmd)
+                cprint(chan.recv(1024).decode() + '\n')
+            else:
+                chan.send('exit')
+                cprint('[*] Exiting', Color.MSG)
+                sess.close()
+                raise Exception('exit')
+        except KeyboardInterrupt:
+            sess.close()
+
+
 if __name__=='__main__':
     try:
         global args
         args = cli()
         if args.server:
-            app = Server()
+            run_server()
         else:
             app = Client()
-        app.execute()
+            app.execute()
     except KeyboardInterrupt:
         cprint('[*] Keyboard interrupt detected, aborting', Color.ERR)
         if not args.server:
             app.client.close()
         sys.exit(1)
+    except Exception as e:
+        cprint('[!] Unhandled exception: {}'.format(e), Color.ERR)
+        try:
+            sess.close()
+        except:
+            pass
+        if args.debug:
+            raise
